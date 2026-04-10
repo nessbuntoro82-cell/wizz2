8b78c837066b13204d38f0df352fd0e2f6f9d824
nessbuntoro82-cell <nessbuntoro82@gmail.com>
2026-04-10 10:22:06 +0700

    Change Auto Sell Threshold from 'Legendary' to 'mythic'

--- script.lua
+++ script.lua
@@ -37,7 +37,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Selling"] = {
         ["Auto Sell"] = true,
-        ["Auto Sell Threshold"] = "Legendary",
+        ["Auto Sell Threshold"] = "mythic",
         ["Auto Sell Every"] = 100,
     },
     ["Doing Quest"] = {

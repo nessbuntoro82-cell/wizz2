7ef1b6692ab3b36826818c2569fce52055ce3237
nessbuntoro82-cell <nessbuntoro82@gmail.com>
2026-04-12 15:54:07 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -29,6 +29,7 @@ _G.FishItConfig = _G.FishItConfig or {
         },
         ["Item Name"] = {
             "Evolved Enchant Stone",
+            "Eggy Enchant Stone",
         },
     },
     ["Farm Coin Only"] = {
